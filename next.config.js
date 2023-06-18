2c640b078b7ff7c7ebb950e485e15a0de100c33b
utifmd <utifmd@gmail.com>
2023-06-18 16:18:26 +0700

    allow require cors #1

--- next.config.js
+++ next.config.js
@@ -7,10 +7,11 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Content-Security-Policy", value: "upgrade-insecure-requests" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Token, X-Api-Version" },
         ]
